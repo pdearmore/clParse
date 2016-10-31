51127efe5eb4863520000e98d5ee7d92e7e65bf8
Philip Dearmore <dearmore@gmail.com>
2016-10-31 10:24:27 -0400

    Added some structure

---
+++ clParse/CommandLine/Argument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using clParse.CommandLine.Interfaces;
+
+namespace clParse.CommandLine
+{
+    public abstract class Argument : IArgument
+    {
+        public string Name { get; set; }
+        public string Help { get; set; }
+        public string Example { get; set; }
+        public string Summary { get; set; }
+
+        public abstract void ProcessArgument(IEnumerable<IArgument> args);
+    }
+}
---
+++ clParse/CommandLine/Interfaces/IArgument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clParse.CommandLine.Interfaces
+{
+    public interface IArgument
+    {
+        string Name { get; set; }
+        string Help { get; set; }
+        string Example { get; set; }
+        string Summary { get; set; }
+
+        void ProcessArgument(IEnumerable<IArgument> args);
+    }
+}
